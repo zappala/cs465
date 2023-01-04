19b0c1be13d7fee65a0939cd75bc19d492028943
Daniel Zappala <daniel.zappala@gmail.com>
2023-01-04 15:00:51 -0700

    Added photo of James

--- dodo.py
+++ dodo.py
@@ -4,6 +4,6 @@ def task_install():
 #        'actions': ['python app.py build','cp htaccess build/.htaccess','rsync -rav --delete build/* zappala@cs465.byu.edu:/var/www/cs465.byu.edu'],
 #        'actions': ['python app.py build','cp htaccess build/.htaccess','rsync -rav --delete build/* fred@cs465.byu.edu:/var/www/cs465.byu.edu'],
         #'actions': ['python app.py build','cp htaccess build/.htaccess','rsync --dry-run --exclude build/winter-2022 --exclude build/spring-2022 -rav build/* fred@cs465.byu.edu:/var/www/cs465.byu.edu'],
-        'actions': ['python app.py build','cp htaccess build/.htaccess','rsync --exclude build/winter-2022 --exclude build/spring-2022 -rav build/* fred@cs465.byu.edu:/var/www/cs465.byu.edu'],
+        'actions': ['python app.py build','cp htaccess build/.htaccess','rsync --exclude build/winter-2022 --exclude build/spring-2022 -rav build/* zappala@cs465.byu.edu:/var/www/cs465.byu.edu'],
 #        'actions': ['python app.py build','cp htaccess build/.htaccess','rsync -rav --delete build/* seamons@cs465.byu.edu:/var/www/cs465.byu.edu'],        
         }
